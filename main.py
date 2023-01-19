7f73eff3cf4b46bc89b39d78390229e4a52f280f
Brett Miller <brett.miller47@yahoo.com>
2023-01-19 15:40:58 -0500

    added function descriptions

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from segno import helpers
 
 
 def create_qr_location(latitude, longitude, name):
+    """
+    Creates a .png QR code for a location.  The QR code will automatically open the maps app on mobile.
+    :param latitude: location's latitude
+    :param longitude: location's longitude
+    :param name: the location's name
+    :return: a file named 'location-{name}.png'
+    """
     # store the latitude and longitude in variable 'geo_uri'
     geo_uri = helpers.make_geo_data(latitude, longitude)
     geo_uri
@@ -18,6 +25,12 @@ def create_qr_location(latitude, longitude, name):
 
 
 def create_qr_wifi(ssid, password):
+    """
+    Creates a .png QR code for wifi access.
+    :param ssid: the wifi's ssid
+    :param password: the password for the wifi's ssid
+    :return: a file named '{ssid}-wifi-access.png'
+    """
     qrcode = helpers.make_wifi(ssid=ssid, password=password, security='WPA')
     qrcode.designator
     '3-M'
@@ -25,6 +38,14 @@ def create_qr_wifi(ssid, password):
 
 
 def create_qr_contact(name, email, phone, url=''):
+    """
+    Creates a .png QR code for contact info.
+    :param name: contact's name
+    :param email: contact's email
+    :param phone: contact's phone number
+    :param url: contact's personal site
+    :return: a file named 'contact-{name}.png'
+    """
     qrcode = helpers.make_mecard(name=name, email=email, phone=phone, url=url)
     qrcode.designator
     '4-H'
